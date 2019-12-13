b030b32901eccb6dfc2bba315096479da071ff0a
Masahiro Fukuda <masahif@gmail.com>
2019-12-13 11:28:47 +0900

    add build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+MD_TO_PDF="`which md-to-pdf`"
+if [ -f ${MD_TO_PDF} ]; then
+  echo "using ${MD_TO_PDF}"
+else
+  npm i -g md-to-pdf
+fi
+
+md-to-pdf README.md `date "+%Y%m%d-RX_constitution.pdf"`
